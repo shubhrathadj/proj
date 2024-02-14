8a48742da48a2aae3b376a1864a6872005e8b12c
shubhrathadj <shubhrathadjshubha@gmail.com>
2024-02-14 14:54:47 +0530

    start

---
+++ src/main/java/com/bootstrap/stdmang/App.java
@@ -0,0 +1,13 @@
+package com.bootstrap.stdmang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
